8f2ae89309fee022c4a9981e34a2446526d93c4e
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-04-26 12:38:21 -0300

    Update imagens.js

--- Códigos/imagens.js
+++ Códigos/imagens.js
@@ -10,7 +10,7 @@ let imagemCarro3;
 function preLoad(){
     imagemDaEstrada = loadImage("imagens/estrada.png");
     imagemDoAtor = loadImage("imagens/ator-1.png");
-    imagemCarro = loadImage("Imagens/carro-1.png");
-    imagemCarro2 = loadImage("Imagens/carro-2.png");
-    imagemCarro3 = loadImage("Imagens/carro-3.png");
+    imagemCarro = loadImage("imagens/carro-1.png");
+    imagemCarro2 = loadImage("imagens/carro-2.png");
+    imagemCarro3 = loadImage("imagens/carro-3.png");
 }
