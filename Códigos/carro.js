1ad3c15d3a5c84744a74d75f76172155bff3e8bf
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-04-26 12:36:56 -0300

    Update

--- Códigos/carro.js
+++ Códigos/carro.js
@@ -2,10 +2,22 @@
 let xCarro = 600;
 let yCarro = 40;
 
+// Carro 2
+let xCarro2 = 600;
+let yCarro2 = 96;
+
+//Carro 3
+let xCarro3 = 600;
+let yCarro3 = 150;
+
 function mostraCarro(){
     image(imagemCarro, xCarro, yCarro, 50, 40);
+    image(imagemCarro2, xCarro2, yCarro2, 50, 40);
+    image(imagemCarro3, xCarro3, yCarro3, 50, 40);
 }
 
 function movimentaCarro(){
     xCarro -= 2;
+    xCarro2 -= 5;
+    xCarro3 -= 3.5;
 }
--- Códigos/imagens.js
+++ Códigos/imagens.js
@@ -3,10 +3,14 @@
 let imagemDaEstrada;
 let imagemDoAtor;
 let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
 
 
 function preLoad(){
     imagemDaEstrada = loadImage("imagens/estrada.png");
     imagemDoAtor = loadImage("imagens/ator-1.png");
     imagemCarro = loadImage("Imagens/carro-1.png");
+    imagemCarro2 = loadImage("Imagens/carro-2.png");
+    imagemCarro3 = loadImage("Imagens/carro-3.png");
 }
