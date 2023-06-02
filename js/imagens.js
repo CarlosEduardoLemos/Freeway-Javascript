a86b49db5029d2c8ba2441ed001ca05fcad3a529
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-06-02 20:34:35 -0300

    Update imagens.js

--- js/imagens.js
+++ js/imagens.js
@@ -14,11 +14,11 @@ let somDoPonto;
 
 
 function preLoad(){
-    imagemDaEstrada = loadImage("imagens/estrada.png");
-    imagemDoAtor = loadImage("imagens/ator-1.png");
-    imagemCarro = loadImage("imagens/carro-1.png");
-    imagemCarro2 = loadImage("imagens/carro-2.png");
-    imagemCarro3 = loadImage("imagens/carro-3.png");
+    imagemDaEstrada = loadImage("../imagens/estrada.png");
+    imagemDoAtor = loadImage("../imagens/ator-1.png");
+    imagemCarro = loadImage("../imagens/carro-1.png");
+    imagemCarro2 = loadImage("../imagens/carro-2.png");
+    imagemCarro3 = loadImage("../imagens/carro-3.png");
     let imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3, imagemCarro, imagemCarro2, imagemCarro3]
 
     somDaTrilha = loadSound("sons/trilha.mp3");
